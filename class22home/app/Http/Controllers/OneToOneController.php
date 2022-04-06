99401db5888e30efbbce9c28628ad4d528ba52bb
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-04-06 06:28:08 +0600

    1to1 relationship user to profile

---
+++ class22home/app/Http/Controllers/OneToOneController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+// use App\Models\User;
+use Illuminate\Http\Request;
+
+class OneToOneController extends Controller
+{
+    function index() {
+        // $users = User::with('profile')->get(); // egar log
+        // $users = User::get();
+        // $users-> load('profile'); // lag load
+
+        // $users = User::whereHas('profile', function ($q) {
+        //     return $q->where('zip_code', '>', 200);
+        // })->get();
+        // $users = User::whereHas('profile', function ($q) {
+        //     return $q->whereNotNull('zip_code');
+        // })->get();
+
+        // $users = User::doesntHave('profile')->get();
+
+        // $users = User::whereDoesntHave('profile', function ($q) {
+        //     return $q->where('name', 'like', '%a');
+        // })->get();
+        // return view('relationship.one-to-one', compact('users'));
+
+        ### The Tnd user to profile  Relationship ###
+
+        $profiles = Profile::get();
+        return view('relationship.one-to-one', compact('profiles'));
+
+        ### The Tnd profile to user Relationship ###
+    }
+}
---
+++ class22home/resources/views/relationship/one-to-one.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>one-to-one</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+</head>
+<body>
+    <h2 class="text-center">Class 22 | Eloquent Relationships | One To One</h2>
+    <hr>
+    <div class="container">
+        <!-- <table class="table table-bordered">
+            <tr>
+                <th>S.No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Zip Code</th>
+            </tr>
+           @foreach ($users as $user)
+
+            <tr>
+                <td>{{ $loop->index +1 }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->profile->address ?? 'Null' }}</td>
+                <td>{{  $user->profile->zip_code ?? 'Nill' }}</td>
+            </tr>
+
+            @endforeach
+        </table> -->
+        <hr>
+        <table class="table table-bordered">
+            <tr>
+                <th>S.No</th>
+                <th>id</th>
+                <th>userName</th>
+                <th>Address</th>
+            </tr>
+            @foreach ($profiles as $profile)
+
+            <tr>
+                <td>{{ $loop->index +1 }}</td>
+                <td>{{ $profile->id }}</td>
+                <td>{{ $profile->user }}</td>
+                <td>{{ $profile->address }}</td>
+            </tr>
+
+            @endforeach
+        </table>
+    </div>
+</body>
+</html>
