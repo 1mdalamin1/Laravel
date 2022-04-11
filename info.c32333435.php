265868efcfa370b919d00bc5371a63a2b3b2bb22
MD AL AMIN <01mdalamin1@gmail.com>
2022-04-11 16:17:47 +0600

    class 32 start now

--- info.c32333435.php
+++ info.c32333435.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 
-01:40s / 21:27s  (32)==> https://www.youtube.com/watch?v=COVYSL23xkE&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=23
+02:40s / 21:27s  (32)==> https://www.youtube.com/watch?v=COVYSL23xkE&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=23
 
 --S-t-a-r-t-->  Class 32 | Artisan Console | Task Scheduling <--S-t-a-r-t-- 
 ___________________________________________________________
@@ -11,7 +11,7 @@ cd c32-35
  
 php artisan serve
 
-php artisan make:factory Student || php artisan db:seed
+php artisan make:command CustomCommand || php artisan db:seed
 php artisan make:controller CustomerController
 __________________________________________________________
 
