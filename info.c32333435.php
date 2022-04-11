9d7459971a22ddb55a9222c638e41e5092965385
MD AL AMIN <01mdalamin1@gmail.com>
2022-04-11 13:26:26 +0600

    Class 32 Start Now

---
+++ info.c32333435.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+
+01:40s / 21:27s  (32)==> https://www.youtube.com/watch?v=COVYSL23xkE&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=23
+
+--S-t-a-r-t-->  Class 32 | Artisan Console | Task Scheduling <--S-t-a-r-t-- 
+___________________________________________________________
+composer create-project laravel/laravel:^8.0 c32-35
+ 
+cd c32-35
+ 
+php artisan serve
+
+php artisan make:factory Student || php artisan db:seed
+php artisan make:controller CustomerController
+__________________________________________________________
+
+
+
+
+
+
+
+*/
