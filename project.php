72af945f250f1774496ba117b6ae0ac94d6e3bc9
MD AL AMIN <01mdalamin1@gmail.com>
2022-04-14 15:15:40 +0600

    Project Planning | E-commerce

---
+++ project.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+13:00s/1:01:09s => https://www.youtube.com/watch?v=c6ujS1awEaQ&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=27
+
+php artisan = pa
+___________________________________________________________
+composer create-project laravel/laravel:^8.0 project
+ 
+cd project
+ 
+php artisan serve
+
+
+*/
+
+
+
