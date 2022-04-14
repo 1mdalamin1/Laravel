286e6ffc71920db3cca81fefbf02350599baaa3a
MD AL AMIN <01mdalamin1@gmail.com>
2022-04-14 16:10:44 +0600

    project start now

--- project.php
+++ project.php
@@ -1,8 +1,10 @@
 <?php 
 /*
-13:00s/1:01:09s => https://www.youtube.com/watch?v=c6ujS1awEaQ&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=27
+20:55s/1:01:09s => https://www.youtube.com/watch?v=c6ujS1awEaQ&list=PLnhrpn1DBwx6MadqSAvKIae1nx0PC-kiH&index=27
 
 php artisan = pa
+
+36 | Project Planning | => https://drawsql.app/vir-za/diagrams/project#
 ___________________________________________________________
 composer create-project laravel/laravel:^8.0 project
  
