2f013e5d32deb9204f00bba243733e0bdc927aeb
tanvironamica <tanvironamica@gmail.com>
2023-03-19 00:00:02 +0600

    add laravel 10 crud

---
+++ info.php
@@ -0,0 +1,36 @@
+<?php
+// Silence is golden.
+/**
+ * https://fajarwz.com/blog/laravel-10-crud-and-image-upload-tutorial-with-laravel-breeze-and-repo-example/
+ * Laravel 10 CRUD with Breeze
+ * 
+ * composer create-project --prefer-dist laravel/laravel:^10 crud-laravel10
+ * composer create-project --prefer-dist laravel/laravel:^10 laravel10
+ * # create a database named = crud-laravel10 & set .env file
+ * composer require laravel/breeze --dev
+ * php artisan breeze:install
+ * 
+ * 
+ * php artisan make:migration "create posts table"
+ * php artisan migrate
+ * php artisan make:controller PostController --resource
+ * php artisan make:request Post/StoreRequest
+ * php artisan make:request Post/UpdateRequest
+ * php artisan make:model Post
+ * php artisan storage:link
+ * npx tailwindcss -i ./resources/css/app.css -o ./resources/css/main.css --watch
+ * 
+ * 
+ * 
+ * npm install 
+ * npm run dev
+ * 
+ * php artisan serve
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
